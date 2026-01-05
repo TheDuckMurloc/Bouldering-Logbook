7ff7e6a8089fce78904eee73e3e24f205b8a0460
Myron Bachynskyy <myronbach07@gmail.com>
2026-01-05 17:13:37 +0100

    Changed identifier for user queries

--- boulderlogbook/src/main/resources/db/migration/V4__insert_users.sql
+++ boulderlogbook/src/main/resources/db/migration/V4__insert_users.sql
@@ -1,2 +1,2 @@
-INSERT INTO [User] (UserID, name, email, PasswordHash, JoinDate, ProfilePhoto) VALUES
+INSERT INTO "User" (UserID, name, email, PasswordHash, JoinDate, ProfilePhoto) VALUES
 (1, 'Myron Bachynskyy', 'myron@gmail.com', 'password_hash', '2024-01-15 00:00:00', NULL);
