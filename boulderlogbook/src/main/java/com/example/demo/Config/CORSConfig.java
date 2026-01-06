ec6e8f90c653f59354f73a323b4a98af93101152
Myron Bachynskyy <myronbach07@gmail.com>
2026-01-06 11:28:45 +0100

    Added CORSConfig

---
+++ boulderlogbook/src/main/java/com/example/demo/Config/CORSConfig.java
@@ -0,0 +1,20 @@
+package main.java.com.example.demo.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://bouldering-logbook-user-frontend.onrender.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
